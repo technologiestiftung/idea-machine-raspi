da65693c7b1d5804f10b658d8e2587ccb913e9a3
Luise Brandenburger <luise.brandenburger@ts.berlin>
2026-05-04 16:23:23 +0200

    feat: add handle exit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import threading
 import time
+import signal
+import sys
 
 import RPi.GPIO as GPIO
 
@@ -10,6 +12,14 @@ from mqtt_handler import setup_mqtt
 from print_handler import handle_print
 from state import button_pressed, get_dice_begriff, state
 from utils import blink
+
+def handle_exit(signum, frame):
+    print("Signal empfangen, räume GPIO auf...")
+    GPIO.cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, handle_exit)
+signal.signal(signal.SIGINT, handle_exit)
 
 #Thread-Setup for blinking LED
 blink_event = threading.Event()
